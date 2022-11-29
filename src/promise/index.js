ad053782de2ecf88e87b98773937c646e4a58c56
Milton Hernandez <cariasmilton2002@gmal.com>
2022-11-29 16:31:31 -0600

    promise

---
+++ src/promise/index.js
@@ -0,0 +1,32 @@
+import fetch from "node-fetch";
+/*
+const API =  "https://api.escuelajs.co/api/v1";
+
+function fetchData(urlApi){
+    return fetch(urlApi);
+}
+
+fetchData(`${API}/products`)
+    .then(response => response.json())
+    .then(products => {
+        console.log(products);
+    })
+    .then(()=>{
+        console.log("hshahfh");
+    })
+    .catch(error => console.log(error));
+*/
+
+fetchData(`${API}/products`)
+    .then(response => response.json())
+    .then(products => {
+        return fetchData(`${API}/products/${products[0].id}`);
+    })
+    .then(response => response.json())
+    .then(products =>{
+        return fetchData(`${API}/categoris/${products.category.id}`)
+    })
+    .then(response => response.json())
+    .then(category =>{
+        console.log(category.name);
+    })
